e98170b123b0b807831e3b75c3b9659575a5f5f6
vasilecerchez <149291482+vasilecerchez@users.noreply.github.com>
2024-06-16 20:03:58 +0200

    Update settings.py

--- espacenet/espacenet/settings.py
+++ espacenet/espacenet/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = "espacenet.spiders"
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "espacenet (+http://www.yourdomain.com)"
 
-USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+USER_AGENT="Mozilla/5......" # change here.................
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False#True
 
